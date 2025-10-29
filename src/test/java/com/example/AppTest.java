d0e502fa9dc02bfe4a97ed09655399fb04c1177f
VinayGone2006 <vinaygone28@gmail.com>
2025-10-29 13:35:48 +0530

    Added Java Maven project for Jenkins pipeline

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testPrint() {
+        System.out.println("Running test from Jenkins...");
+    }
+}
+
